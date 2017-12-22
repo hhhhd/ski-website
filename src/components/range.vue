4cf627ea991ed93f9ccc05b99de1f3af23d05dc1
Korui <a1665083753@gmail.com>
2017-12-22 22:53:55 +0800

    feat:range

--- src/components/range.vue
+++ src/components/range.vue
@@ -1,4 +1,26 @@
 <template>
+  <section>
+    <h1>range</h1>
+    <p>两个大小：big、small</p>
+    <p>属性：type（big，small）setValue（number）：设定初始值 setStep（number）设置每一步走多大</p>
+    <my-code>
+      <pre slot="codeText">
+&lt<span class="skiblue">template</span>&gt
+  &lt<span class="skiblue">my-range</span> <span class="red">type</span>=<span class="blue">"big || small"</span> <span class="red">setValue</span>=<span class="blue">"(number)"</span> <span class="red">setStep</span>=<span class="blue">"(number)"</span>&gt&lt/<span class="skiblue">my-range</span>&gt
+&lt/<span class="skiblue">template</span>&gt
+
+&lt<span class="skiblue">script</span>&gt
+<span class="skiblue">import</span> {<span class="pink"> range </span>} <span class="skiblue">from 'ski-ui'</span>
+
+<span class="pink">export default</span> {
+  <span class="skiblue">components:</span> {
+    <span class="skiblue">MyRange: range</span>
+  }
+}
+&lt/<span class="skiblue">script</span>&gt
+      </pre>
+    </my-code>
+  </section>
 </template>
 
 <script>
