672b1cd7d3124c870f4bb00f25914031560679d4
Korui <a1665083753@gmail.com>
2017-12-24 23:06:12 +0800

    feat: popup

--- src/components/popup.vue
+++ src/components/popup.vue
@@ -1,4 +1,28 @@
 <template>
+  <section>
+    <h1>popup</h1>
+    <p>三种类型：top，middle，right</p>
+    <p>属性：type（top，middle，right） msg（string） show（true || false）</p>
+    <my-code>
+      <pre slot="codeText">
+&lt<span class="skiblue">template</span>&gt
+  &lt<span class="skiblue">my-popup</span> <span class="red">type</span>=<span class="blue">"top"</span> <span class="red">msg</span>=<span class="blue">"这是一个消息"</span> <span class="red">show</span>=<span class="blue">"true"</span>&gt&lt/<span class="skiblue">my-popup</span>&gt
+  &lt<span class="skiblue">my-popup</span> <span class="red">type</span>=<span class="blue">"middle"</span> <span class="red">msg</span>=<span class="blue">"这是一个消息"</span> <span class="red">show</span>=<span class="blue">"true"</span>&gt&lt/<span class="skiblue">my-popup</span>&gt
+  &lt<span class="skiblue">my-popup</span> <span class="red">type</span>=<span class="blue">"right"</span> <span class="red">msg</span>=<span class="blue">"这是一个消息"</span> <span class="red">show</span>=<span class="blue">"true"</span>&gt&lt/<span class="skiblue">my-popup</span>&gt
+&lt/<span class="skiblue">template</span>&gt
+
+&lt<span class="skiblue">script</span>&gt
+<span class="skiblue">import</span> {<span class="pink"> popup </span>} <span class="skiblue">from 'ski-ui'</span>
+
+<span class="pink">export default</span> {
+  <span class="skiblue">components:</span> {
+    <span class="skiblue">Mypopup: popup</span>
+  }
+}
+&lt/<span class="skiblue">script</span>&gt
+      </pre>
+    </my-code>
+  </section>
 </template>
 
 <script>
