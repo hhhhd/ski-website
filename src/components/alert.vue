38e9dba16c7ab3fcb1d6690fc319343cfc71f56d
Korui <a1665083753@gmail.com>
2017-12-26 22:30:31 +0800

    feat:spinner

--- src/components/alert.vue
+++ src/components/alert.vue
@@ -1,11 +1,11 @@
 <template>
   <section>
     <h1>Alert</h1>
-    <p>属性：msg（显示弹出的文字），弹出后点击确定直接消失，需要自己通过 v-show 控制显示</p>
+    <p>属性：msg（显示弹出的文字）show(true || false)</p>
     <my-code>
       <pre slot="codeText">
 &lt<span class="skiblue">template</span>&gt
-  &lt<span class="skiblue">my-alert</span> <span class="red">msg</span>=<span class="white">"msg"</span> <span class="red">v-show</span>=<span class="skiblue">"true"</span>&gt&lt/<span class="skiblue">my-alert</span>&gt
+  &lt<span class="skiblue">my-alert</span> <span class="red">msg</span>=<span class="white">"msg"</span> <span class="red">show</span>=<span class="skiblue">"true"</span>&gt&lt/<span class="skiblue">my-alert</span>&gt
 &lt/<span class="skiblue">template</span>&gt
 
 &lt<span class="skiblue">script</span>&gt
--- src/components/spinner.vue
+++ src/components/spinner.vue
@@ -1,4 +1,30 @@
 <template>
+  <section>
+    <h1>spinner</h1>
+    <p>动画加载</p>
+    <p>type(round, snake, roulette, window, wave)</p>
+    <my-code>
+      <pre slot="codeText">
+&lt<span class="skiblue">template</span>&gt
+  &lt<span class="skiblue">my-spinner</span> <span class="red">type</span>=<span class="blue">"snake"</span>&gt&lt/<span class="skiblue">my-spinner</span>&gt
+  &lt<span class="skiblue">my-spinner</span> <span class="red">type</span>=<span class="blue">"round"</span>&gt&lt/<span class="skiblue">my-spinner</span>&gt
+  &lt<span class="skiblue">my-spinner</span> <span class="red">type</span>=<span class="blue">"roulette"</span>&gt&lt/<span class="skiblue">my-spinner</span>&gt
+  &lt<span class="skiblue">my-spinner</span> <span class="red">type</span>=<span class="blue">"window"</span>&gt&lt/<span class="skiblue">my-spinner</span>&gt
+  &lt<span class="skiblue">my-spinner</span> <span class="red">type</span>=<span class="blue">"wave"</span>&gt&lt/<span class="skiblue">my-spinner</span>&gt
+&lt/<span class="skiblue">template</span>&gt
+
+&lt<span class="skiblue">script</span>&gt
+<span class="skiblue">import</span> {<span class="pink"> spinner </span>} <span class="skiblue">from 'ski-ui'</span>
+
+<span class="pink">export default</span> {
+  <span class="skiblue">components:</span> {
+    <span class="skiblue">Myspinner: spinner</span>
+  }
+}
+&lt/<span class="skiblue">script</span>&gt
+      </pre>
+    </my-code>
+  </section>
 </template>
 
 <script>
