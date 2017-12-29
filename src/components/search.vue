7ac92718ad7c5a47ad9b8f0976bc476f32c9acf0
Korui <a1665083753@gmail.com>
2017-12-29 22:19:23 +0800

    feat:toast/search

--- src/components/search.vue
+++ src/components/search.vue
@@ -1,4 +1,26 @@
 <template>
+  <section>
+    <h1>search</h1>
+    <p>没有属性，直接使用</p>
+    <p>用 @value 获取到输入的值</p>
+    <my-code>
+      <pre slot="codeText">
+&lt<span class="skiblue">template</span>&gt
+  &lt<span class="skiblue">ski-search</span> <span class="red">@value</span>=<span class="blue">"Event"</span>&gt&lt/<span class="skiblue">ski-search</span>&gt
+&lt/<span class="skiblue">template</span>&gt
+
+&lt<span class="skiblue">script</span>&gt
+<span class="skiblue">import</span> {<span class="pink"> search </span>} <span class="skiblue">from 'ski-ui'</span>
+
+<span class="pink">export default</span> {
+  <span class="skiblue">components:</span> {
+    <span class="skiblue">skiSearch: search</span>
+  }
+}
+&lt/<span class="skiblue">script</span>&gt
+      </pre>
+    </my-code>
+  </section>
 </template>
 
 <script>
--- src/components/toast.vue
+++ src/components/toast.vue
@@ -1,5 +1,45 @@
 <template>
-  <section class="section"></section>
+  <section>
+    <h1>toast</h1>
+    <p>一共两种（middle，bottom）</p>
+    <p>属性：</p>
+    <ul>
+      <li>type
+        <ul>
+          <li>middle</li>
+          <li>bottom</li>
+        </ul> 
+      </li>
+      <li>msg
+        <ul>
+          <li>文本</li>
+        </ul>
+      </li>
+      <li>show
+        <ul>
+          <li>true</li>
+          <li>false</li>
+        </ul>
+      </li>
+    </ul>
+    <my-code>
+      <pre slot="codeText">
+&lt<span class="skiblue">template</span>&gt
+  &lt<span class="skiblue">ski-toast</span> <span class="red">type</span>=<span class="blue">"middle"</span> <span class="red">show</span>=<span class="blue">"true"</span> <span class="red">msg</span>=<span class="blue">"text"</span>&gt&lt/<span class="skiblue">ski-toast</span>&gt
+&lt/<span class="skiblue">template</span>&gt
+
+&lt<span class="skiblue">script</span>&gt
+<span class="skiblue">import</span> {<span class="pink"> toast </span>} <span class="skiblue">from 'ski-ui'</span>
+
+<span class="pink">export default</span> {
+  <span class="skiblue">components:</span> {
+    <span class="skiblue">skiToast: toast</span>
+  }
+}
+&lt/<span class="skiblue">script</span>&gt
+      </pre>
+    </my-code>
+  </section>
 </template>
 
 <<script>
