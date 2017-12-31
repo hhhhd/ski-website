ec5cc1534cd6a32d396ecd450285289d30dea6db
Korui <a1665083753@gmail.com>
2017-12-31 22:10:08 +0800

    feat:actionSheet

--- src/components/actionSheet.vue
+++ src/components/actionSheet.vue
@@ -1,4 +1,26 @@
 <template>
+  <section>
+    <h1>actionSheet</h1>
+    <p>调用相机进行拍照取照片</p>
+    <p>属性：show（true || false）</p>
+    <my-code>
+      <pre slot="codeText">
+&lt<span class="skiblue">template</span>&gt
+  &lt<span class="skiblue">ski-actionSheet</span> <span class="red">show</span>=<span class="blue">"true"</span>&gt&lt/<span class="skiblue">ski-actionSheet</span>&gt
+&lt/<span class="skiblue">template</span>&gt
+
+&lt<span class="skiblue">script</span>&gt
+<span class="skiblue">import</span> {<span class="pink"> actionSheet </span>} <span class="skiblue">from 'ski-ui'</span>
+
+<span class="pink">export default</span> {
+  <span class="skiblue">components:</span> {
+    <span class="skiblue">ski-ActionSheet: actionSheet</span>
+  }
+}
+&lt/<span class="skiblue">script</span>&gt
+      </pre>
+    </my-code>
+  </section>
 </template>
 
 <script>
