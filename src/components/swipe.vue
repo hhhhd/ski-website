447175ad38fbccf9488d0b5e435cc7da6e921091
Korui <a1665083753@gmail.com>
2018-01-03 22:41:03 +0800

    feat:swipe

--- src/components/swipe.vue
+++ src/components/swipe.vue
@@ -1,4 +1,26 @@
 <template>
+  <section>
+    <h1>swipe</h1>
+    <p>滚动导航栏</p>
+    <p>暂时没有属性：后续添加</p>
+    <my-code>
+      <pre slot="codeText">
+&lt<span class="skiblue">template</span>&gt
+  &lt<span class="skiblue">ski-swipe</span>&gt&lt/<span class="skiblue">ski-swipe</span>&gt
+&lt/<span class="skiblue">template</span>&gt
+
+&lt<span class="skiblue">script</span>&gt
+<span class="skiblue">import</span> {<span class="pink"> swipe </span>} <span class="skiblue">from 'ski-ui'</span>
+
+<span class="pink">export default</span> {
+  <span class="skiblue">components:</span> {
+    <span class="skiblue">ski-swipe: swipe</span>
+  }
+}
+&lt/<span class="skiblue">script</span>&gt
+      </pre>
+    </my-code>
+  </section>
 </template>
 
 <script>
