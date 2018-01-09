1f4f1c0344dd7ab1e0398bb1f68081d09d5a455e
Korui <a1665083753@gmail.com>
2018-01-09 23:36:12 +0800

    feat:checklist

--- src/components/checklist.vue
+++ src/components/checklist.vue
@@ -1,4 +1,32 @@
 <template>
+  <section>
+    <h1>checklist</h1>
+    <p>属性</p>
+    <ul>
+      <li>getArr
+        <ul>
+          <li>Array(condition:是否选中 / forbidden:是否禁止改变)</li>
+        </ul>
+      </li>
+    </ul>
+    <ski-code>
+      <pre slot="codeText">
+&lt<span class="skiblue">template</span>&gt
+  &lt<span class="skiblue">ski-checklist</span> <span class="red">:getArr</span>=<span class="blue">"[{condition: true, orbidden: false}, {condition: false, forbidden: true}]"</span>&gt&lt/<span class="skiblue">ski-checklist</span>&gt
+&lt/<span class="skiblue">template</span>&gt
+
+&lt<span class="skiblue">script</span>&gt
+<span class="skiblue">import</span> {<span class="pink"> checklist </span>} <span class="skiblue">from 'ski-ui'</span>
+
+<span class="pink">export default</span> {
+  <span class="skiblue">components:</span> {
+    <span class="skiblue">skiChecklist: checklist</span>
+  }
+}
+&lt/<span class="skiblue">script</span>&gt
+      </pre>
+    </ski-code>
+  </section>
 </template>
 
 <script>
